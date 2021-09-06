0266ad822d27ee92d38b9ac4716ad10bc747b31a
Swordfish Hoplon <swordfish@hoplon.com>
2021-09-06 15:56:18 -0300

    Create EndMatch.js
    Adding EndMatch

---
+++ EndMatch.js
@@ -0,0 +1,25 @@
+handlers.EndMatch = function (args, context) {
+  // stage	    Stage
+  // lifeItem	    Item
+  // coinsAmount	int
+
+  MeowchemyCloudScript.init();
+
+  stageName = "Stage" + args.stage.stageId;
+
+  const dataPayload = {};
+  dataPayload[stageName] = args.stage;
+
+  server.UpdateUserData({
+    PlayFabId: currentPlayerId,
+    Data: dataPayload,
+  });
+
+  if (undefined !== args.coinsAmount)
+    MeowchemyCloudScript.updateVirtualCurrency(args.coinsAmount);
+
+  if (undefined != args.items)
+    MeowchemyCloudScript.updateItemsIventory(args.items);
+
+  return args;
+};
