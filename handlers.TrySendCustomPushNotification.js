2f855cdf35ea9cd533ac58cc3f0e55e996129be0
Dorime <eduardo.silva@hoplon.com>
2021-09-22 16:05:30 -0300

    Added TrySendCustomPushNotification

--- handlers.TrySendCustomPushNotification.js
+++ handlers.TrySendCustomPushNotification.js
@@ -2,36 +2,17 @@ handlers.TrySendCustomPushNotification = function (args, context) {
 
     let userLifeAmount = MeowchemyCloudScript.getCurrentUserLifeAmount();
     let userLifeMaxAmount = MeowchemyCloudScript.getLifeMaxStack();
+    let userNextRewardTime = CloudScriptLib.getUserData(["NextAdRewardCurrencyTime"]);
     let utcNow = ((new Date().getTime() * 10000) + 621355968000000000);
 
-    //log.debug("userLifeAmount");
-    //log.debug(userLifeAmount);
-    //log.debug("userLifeMaxAmount");
-    //log.debug(userLifeMaxAmount);
-
     if (userLifeAmount == userLifeMaxAmount) {
-        //log.debug("userLifeAmount and userLifeMaxAmount are equals, sending push notification")
         CloudScriptLib.SendPushNotificationFromTemplate("6b6a3814-99df-4387-9469-1f4f083a41b6")
     }
-
-    let userNextRewardTime = CloudScriptLib.getUserData(["NextAdRewardCurrencyTime"]);
-
-    log.debug("userNextRewardTime");
-    log.debug(userNextRewardTime);
-    log.debug("utcNow");
-    log.debug(utcNow);
-
-    log.debug(userNextRewardTime.Data.NextAdRewardCurrencyTime !== undefined);
-    log.debug(userNextRewardTime.Data.NextAdRewardCurrencyTime.Value !== undefined);
-    log.debug(userNextRewardTime.Data.NextAdRewardCurrencyTime.Value < utcNow);
-
-
+    
     if (userNextRewardTime.Data.NextAdRewardCurrencyTime !== undefined
         && userNextRewardTime.Data.NextAdRewardCurrencyTime.Value !== undefined
         && userNextRewardTime.Data.NextAdRewardCurrencyTime.Value < utcNow) {
 
-        log.debug("NextAdRewardCurrencyTime is lower then utcNow, sending push notification")
-    }
-
-    // SendPushnotification
+        CloudScriptLib.SendPushNotificationFromTemplate("7de32b12-53e4-4e0e-8715-57a014f1adab")
+    }    
 }
