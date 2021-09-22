78467c2dbf1282c6a726753244640649194b7f6c
Dorime <eduardo.silva@hoplon.com>
2021-09-22 14:43:25 -0300

    WIP TrySendCustomPushNotification

--- handlers.TrySendCustomPushNotification.js
+++ handlers.TrySendCustomPushNotification.js
@@ -3,6 +3,11 @@ handlers.TrySendCustomPushNotification = function (args, context) {
     let userLifeAmount = MeowchemyCloudScript.getCurrentUserLifeAmount();
     let userLifeMaxAmount = MeowchemyCloudScript.getLifeMaxStack();
 
+    log.debug("userLifeAmount");
+    log.debug(userLifeAmount);
+    log.debug("userLifeMaxAmount");
+    log.debug(userLifeMaxAmount);
+
     if (userLifeAmount === userLifeMaxAmount) {
         log.debug("userLifeAmount and userLifeMaxAmount are equals, sending push notification")
         CloudScriptLib.SendPushNotificationFromTemplate("6b6a3814-99df-4387-9469-1f4f083a41b6")
