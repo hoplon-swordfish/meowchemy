f3cb9dae77e8d8f70e32991ced458c2201a7f632
Dorime <eduardo.silva@hoplon.com>
2021-11-12 13:45:59 -0300

    Add new api: QueueGiftToUser

---
+++ handlers.QueueGiftToUser.js
@@ -0,0 +1,51 @@
+/* 
+    args
+    {
+        GiftItemId: xxx,
+        GiftItemAmount: n
+    }
+*/
+handlers.handlers.QueueGiftToUser = function (args, context)
+{
+    try
+    {
+        let logMessage = ``;
+        logMessage = `QueueGiftToUser::args: ${JSON.stringify(args)}`;
+        log.info(logMessage);
+
+        let ServerGiftQueue = CloudScriptLib.getUserData(["GiftQueue"]);
+        logMessage = `QueueGiftToUser::GiftQueue: ${JSON.stringify(GiftQueue)}`;
+        log.info(logMessage);
+
+        let GiftQueue = [];
+        if (ServerGiftQueue.Data.GiftQueue !== undefined
+            && ServerGiftQueue.Data.GiftQueue.Value !== undefined)
+            GiftQueue = ServerGiftQueue.Data.GiftQueue.Value;
+
+        GiftQueue.push(
+            {
+                GiftItemId: args.GiftItemId,
+                GiftItemAmount: args.GiftItemAmount
+            }
+        );
+
+        logMessage = `QueueGiftToUser::GiftQueue: ${JSON.stringify(GiftQueue)}`;
+        log.info(logMessage);
+
+        server.UpdateUserData(
+            {
+                PlayFabId: currentPlayerId,
+                Data: { "GiftQueue": GiftQueue }
+            });
+    }
+    catch (e)
+    {
+        let rawError = `Raw error message: ${e}`
+        log.error(rawError)
+
+        let error = e.apiErrorInfo.apiError.error;
+        let errorCode = e.apiErrorInfo.apiError.errorCode;
+        let errorMessage = `API Error: ${error}. Error code: ${errorCode}`;
+        log.error(errorMessage);
+    }
+}
