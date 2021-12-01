4e2d193f6748a1eb50e934592dc810b4c060cf09
Swordfish Hoplon <swordfish@hoplon.com>
2021-12-01 16:03:40 -0300

    Save ProgressionLevel on EndMatch

--- handlers.EndMatch.js
+++ handlers.EndMatch.js
@@ -2,13 +2,19 @@ handlers.EndMatch = function (args, context) {
   // stage	    Stage
   // lifeItem	    Item
   // coinsAmount	int
+  // progressionLevel int
 
   MeowchemyCloudScript.init();
 
   stageName = "Stage" + args.stage.stageId;
 
   const dataPayload = {};
+
   dataPayload[stageName] = JSON.stringify(args.stage);
+
+  if (undefined !== args.progressionLevel) {
+    dataPayload["ProgressionLevel"] = args.progressionLevel;
+  }
 
   server.UpdateUserData({
     PlayFabId: currentPlayerId,
